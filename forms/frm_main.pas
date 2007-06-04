8984e8a389e2333c1982d294c1935dcdd5cfb4b1
kerberos002 <kerberos002@arcor.de>
2007-06-04 21:58:48 +0000

    *** empty log message ***

--- forms/frm_main.pas
+++ forms/frm_main.pas
@@ -5,7 +5,7 @@
   Copyright (c) 2004-2007 Oliver Valencia
   Copyright (c) 2002-2004 Oliver Valencia, Oliver Kutsche
 
-  letzte Änderung  03.06.2007
+  letzte Änderung  04.06.2007
 
   Dieses Programm ist freie Software. Sie können es unter den Bedingungen der
   GNU General Public License weitergeben und/oder modifizieren. Weitere
@@ -5333,6 +5333,7 @@ begin
     CDETreeViewPopupN3.Visible := True;
     CDETreeViewPopupNewFolder.Visible := True;
   end;
+  {Darf alte Session importiert werden?}
   Temp := FSettings.DataCD.Multi and FSettings.DataCD.ContinueCD and
           (FSettings.General.Choice = cDataCD);
   CDETreeViewPopupN4.Visible := Temp;
@@ -5441,8 +5442,11 @@ begin
   CheckDataCDFS(False);
   UserAddFolderUpdateTree(CDETreeView);
   GetVolumeInfo(VolInfo);
-  FData.SetCDLabel(VolInfo.Name, FSettings.General.Choice);
-  CDETreeView.Items[0].Text := VolInfo.Name;
+  if VolInfo.Name <> '' then
+  begin
+    FData.SetCDLabel(VolInfo.Name, FSettings.General.Choice);
+    CDETreeView.Items[0].Text := VolInfo.Name;
+  end;
   UpdateGauges;
 end;
 
