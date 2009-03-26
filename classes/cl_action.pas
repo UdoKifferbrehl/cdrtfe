1de487e811197bf95b5c2c8f9989f3aba3e499b4
kerberos002 <kerberos002@arcor.de>
2009-03-26 12:33:33 +0000

    *** empty log message ***

--- classes/cl_action.pas
+++ classes/cl_action.pas
@@ -5,7 +5,7 @@
   Copyright (c) 2004-2009 Oliver Valencia
   Copyright (c) 2002-2004 Oliver Valencia, Oliver Kutsche
 
-  letzte Änderung  22.02.2009
+  letzte Änderung  24.03.2009
 
   Dieses Programm ist freie Software. Sie können es unter den Bedingungen der
   GNU General Public License weitergeben und/oder modifizieren. Weitere
