0fe7672746458ace491eb13c86ff359447e2a849
kerberos002 <kerberos002@arcor.de>
2009-09-10 19:46:55 +0000

    Workaround fur Access Violation, wenn 2. Instanz beendet wird.

--- forms/frm_main.pas
+++ forms/frm_main.pas
@@ -5,7 +5,7 @@
   Copyright (c) 2004-2009 Oliver Valencia
   Copyright (c) 2002-2004 Oliver Valencia, Oliver Kutsche
 
-  letzte Änderung  06.09.2009
+  letzte Änderung  10.09.2009
 
   Dieses Programm ist freie Software. Sie können es unter den Bedingungen der
   GNU General Public License weitergeben und/oder modifizieren. Weitere
@@ -4431,8 +4431,9 @@ begin
   end;
   {In Abhängigkeit der Fensterhöhe FileExplorer zulassen oder nicht}
   CanShowFileExplorer := ((PageControl1.Height - 200) >= 250);
-  MainMenuToggleFileExplorer.Enabled := FFileExplorerShowing or
-                                        CanShowFileExplorer;
+  if not FInstanceTermination then
+    MainMenuToggleFileExplorer.Enabled := FFileExplorerShowing or
+                                          CanShowFileExplorer;
   {gegebenenfalls FileExplorer automatisch ausblenden}
   ShouldNotShowFileExplorer := FFileExplorerShowing
                   and not (CanShowFileExplorer or (PageControl1.Height >= 250));
