a416b4b964d4f2e5bd0cd499d378c00a9018bc08
kerberos002 <kerberos002@arcor.de>
2011-08-15 17:05:29 +0000

    freedb-Infos werden nun auch in die Info-Dateien uebernommen.

--- core/classes/cl_action_daegrabtracks.pas
+++ core/classes/cl_action_daegrabtracks.pas
@@ -5,7 +5,7 @@
   Copyright (c) 2004-2011 Oliver Valencia
   Copyright (c) 2002-2004 Oliver Valencia, Oliver Kutsche
 
-  letzte Änderung  24.04.2011
+  letzte Änderung  15.08.2011
 
   Dieses Programm ist freie Software. Sie können es unter den Bedingungen der
   GNU General Public License weitergeben und/oder modifizieren. Weitere
@@ -125,6 +125,14 @@ begin
     if Bulk or DoCopy then CommandLine := CommandLine + ' -bulk';
     if NoInfoFile
        and not DoCopy then CommandLine := CommandLine + ' -no-infofile';
+    if UseCDDB and not (NoInfoFile or FCompressed) then
+    begin
+      CommandLine := CommandLine + ' cddb=1';
+      if CDDBServer <> '' then
+        CommandLine := CommandLine + ' -cddbp-server=' + CDDBServer;
+      if CDDBPort <> '' then
+        CommandLine := CommandLine + ' -cddbp-port=' + CDDBPort;
+    end;
     if Offset <> ''   then CommandLine := CommandLine + ' -offset ' + Offset;
   end;
   Result := CommandLine;
