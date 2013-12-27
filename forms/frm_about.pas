0e8ce35f9a71da2fae9a7308c44363d99a270cfc
kerberos002 <kerberos002@users.sourceforge.net>
2013-12-27 11:24:58 +0100

    eMail-Adresse aktualisiert.

--- forms/frm_about.pas
+++ forms/frm_about.pas
@@ -5,7 +5,7 @@
   Copyright (c) 2004-2013 Oliver Valencia
   Copyright (c) 2002-2004 Oliver Valencia, Oliver Kutsche
 
-  letzte Änderung  27.08.2013
+  letzte Änderung  27.12.2013
 
   Dieses Programm ist freie Software. Sie können es unter den Bedingungen der
   GNU General Public License weitergeben und/oder modifizieren. Weitere
@@ -82,7 +82,7 @@ const Cdrtfe_Name        = 'cdrtfe';
       Cdrtfe_Copyright   = 'Copyright © 2004-2013  O. Valencia';
       Cdrtfe_Copyright2  = 'Copyright © 2002-2004  O. Valencia, O. Kutsche';
       Cdrtfe_Homepage    = 'http://cdrtfe.sourceforge.net';
-      Cdrtfe_eMail       = 'kerberos002@arcor.de';
+      Cdrtfe_eMail       = 'kerberos002@users.sourceforge.net';
       {$IFDEF TestVersion}
       Cdrtfe_HintTest    = 'Achtung/Attention!' + CRLF +
                            'Dies ist eine Testversion, die noch schwere ' +
