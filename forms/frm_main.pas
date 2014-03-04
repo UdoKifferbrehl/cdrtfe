dd466a00062e434455939b1a8e3fad234e03ea5a
kerberos002 <kerberos002@users.sourceforge.net>
2014-03-04 23:53:24 +0100

    Kommandozeilenoption /img kann jetzt auch mit /load verwendet werden.

--- forms/frm_main.pas
+++ forms/frm_main.pas
@@ -5,7 +5,7 @@
   Copyright (c) 2004-2014 Oliver Valencia
   Copyright (c) 2002-2004 Oliver Valencia, Oliver Kutsche
 
-  letzte Änderung  23.02.2014
+  letzte Änderung  04.03.2014
 
   Dieses Programm ist freie Software. Sie können es unter den Bedingungen der
   GNU General Public License weitergeben und/oder modifizieren. Weitere
@@ -1830,8 +1830,14 @@ begin
     cVideoCD : Path := '';
     cDVDVideo: if DirectoryExists(FileName) and IsValidDVDSource(FileName) then
                                             // temporary Hack
+               begin
                  EditDVDVideoSourcePath.Text := FileName;
-    cCDImage : EditImageIsoPath.Text := FileName;
+                 SetSettings;
+               end;
+    cCDImage : begin
+                 EditImageIsoPath.Text := FileName;
+                 SetSettings;
+               end;
   end;
   {$IFDEF DebugAddFilesDragDrop}
   Deb('Dateiname: ' + FileName, 3);
